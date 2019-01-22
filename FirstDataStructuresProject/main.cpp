63fe07d3cd3215f6529e2455d18a2edecf138daa
Davis, Lauren <ldav7984@740s-pl-109.local>
2019-01-22 13:48:42 -0700

    Initial Commit

---
+++ FirstDataStructuresProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstDataStructuresProject
+//
+//  Created by Davis, Lauren on 1/22/19.
+//  Copyright © 2019 Davis, Lauren. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
